f18e1b5a89412d1d94fdc3ab41f3c6204cb8308b
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-29 12:50:59 +0000

    Adding tag 1.5.1.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-field-images/tags/1.5.1.1@76441 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-field-images.php
@@ -0,0 +1,149 @@
+<?php
+/*
+Plugin Name: Custom Field Images
+Version: 1.5.1.1
+Description: (<a href="edit.php?page=custom-field-images">Manage</a> | <a href="options-general.php?page=custom-field-images">Settings</a>) Easily display images anywhere using custom fields.
+Author: scribu
+Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/projects/custom-field-images.html
+
+Copyright (C) 2008 scribu.net (scribu AT gmail DOT com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class cfImg {
+
+	// Styles for images in feeds
+	var $styles = array(
+		'left' => 'float:left; margin: 0 1em .5em 0;',
+		'center' => 'display:block; margin:0 auto .5em auto;',
+		'right' => 'float:right; margin: 0 0 .5em 1em;'
+	);
+
+	// $post->ID
+	var $id;
+
+	// wp_postmeta -> meta_key
+	var $key = '_cfi_image';
+
+	// Data fields for each image
+	var $data = array(
+		'url' => '',
+		'align' => '',
+		'alt' => '',
+		'link' => ''
+	);
+
+	// Display options
+	var $options = array(
+		'default_align' => 'right',
+		'default_link' => TRUE,
+		'extra_attr' => '',
+
+		'content' => TRUE,
+		'feed' => TRUE,
+		'excerpt' => TRUE
+	);
+
+	function __construct() {
+		$this->options = get_option('cfi_options');
+
+		add_filter('the_excerpt', array(&$this, 'filter'));
+		add_filter('the_content', array(&$this, 'filter'));
+	}
+
+	function filter($content) {
+		$type = substr(current_filter(), 4);
+		$is_feed = is_feed();
+
+		if ( ($is_feed && $this->options['feed']) || (!$is_feed && $this->options[$type]) )
+			return $this->generate() . $content;
+
+		return $content;
+	}
+
+	function load($post_id = '') {
+		global $post;
+
+		$this->id = $post_id ? $post_id : $post->ID;
+
+		$this->data = get_post_meta($this->id, $this->key, TRUE);
+	}
+
+	function generate($post_id = '') {
+		$this->load($post_id);
+
+		$url = $this->data['url'];
+
+		if ( !$url )
+			return;
+
+		// Begin img tag
+		$image.= '<img src="'. $url .'" ';
+
+		// Set alignment
+		$align = $this->data['align'] ? $this->data['align'] : $this->options['default_align'];
+
+		if ( is_feed() )
+			$image .= 'style="' . $this->styles[$align] .'" ';
+		else
+			$image .= 'class="cfi align'. $align .'" ';
+
+		// Set alt text
+		$alt = $this->data['alt'] ? $this->data['alt'] : get_the_title();
+
+		$image .= 'alt="' . $alt . '" ';
+
+		// End img tag
+		$image .= '/>';
+
+		return $this->add_link($image);
+	}
+
+	function add_link($image) {
+		// Sets the link for the image
+
+		$link = $this->data['link'];
+
+		if ( !$link )
+			if ( !$this->options['default_link'] )
+				return $image;
+			else
+				$link = get_permalink($this->id);
+
+		$output = '<a href="'. $link . '"';
+		$output .= ' ' . stripslashes($this->options['extra_attr']);
+		$output .= '>' . $image . '</a>';
+
+		return $output;
+	}
+}
+
+// Init
+if ( is_admin() ) {
+	require_once('inc/admin.php');
+
+	$cfImgAdmin = new cfImgAdmin();
+
+	register_activation_hook(__FILE__, array(&$cfImgAdmin, 'activate'));
+} else
+	$cfImg = new cfImg();
+
+// Template tag
+function custom_field_image() {
+	global $cfImg;
+	echo $cfImg->generate();
+}
+
---
+++ inc/admin.php
@@ -0,0 +1,481 @@
+<?php
+class cfImgAdmin extends cfImg {
+	var $version = '1.5';
+	var $upgrade_from;
+	var $nonce = 'cfi-admin-key';
+
+	function __construct() {
+		add_action('admin_menu', array(&$this, 'page_init'));
+		add_action('admin_menu', array(&$this, 'box_init'));
+		add_action('save_post', array(&$this, 'save'), 1, 2);
+
+		if ( $this->upgrade_from = get_option('cfi_upgrade_from') )
+			add_action('admin_notices', array(&$this, 'warning'));
+	}
+
+	function warning() {
+		$manage_url = 'edit.php?page=custom-field-images';
+
+		if ( strstr($_SERVER['REQUEST_URI'], $manage_url) )
+			return;
+
+		echo '<div class="updated fade"><p><strong>Custom Field Images</strong>: Data upgrade required. Please visit the <a href="' . $manage_url . '">management page</a>.</p></div>';
+	}
+
+// Upgrade options and set upgrade flag
+
+	function activate() {
+		$old_ver = get_option('cfi_version');
+
+		if ( $old_ver == $this->version)
+			return;	// Already done
+
+		if ( $old_ver == '' ) {
+			$old_options = get_option('cfi-show-in');
+			delete_option('cfi-show-in');
+			$old_ver = '1.2';
+		} else
+			$old_options = get_option('cfi_options');
+
+		foreach ( $old_options as $name => $value )
+			$this->options[$name] = $value;
+
+		   add_option('cfi_options', $this->options) or
+		update_option('cfi_options', $this->options);
+
+		   add_option('cfi_version', $this->version, '', 'no') or
+		update_option('cfi_version', $this->version);
+
+		if ( $old_options )	// If not fresh install
+			add_option('cfi_upgrade_from', $old_ver, '', 'no');
+	}
+
+// Management page methods
+
+	function process_posts($action) {
+		$action = strtolower($action);
+
+		switch ($action) {
+			case 'import':
+			case 'export':
+				$r = call_user_func(array(&$this, 'impex'), $action);
+				break;
+			default:
+				$r = call_user_func(array(&$this, $action));
+		}
+
+		if ( $r !== NULL)
+			echo '<div class="updated fade"><p>' . ucfirst(rtrim($action, 'e')) . 'ed <strong>' . $r . '</strong> image(s).</p></div>';
+		else
+			echo '<div class="error"><p>An error has occured.</p></div>';
+	}
+
+// Upgrade methods
+
+	function upgrade() {
+		global $wpdb;
+
+		delete_option('cfi_upgrade_from');
+
+		if ( $this->upgrade_from == '1.2')
+			return $this->upgrade_1_2();
+
+		$this->upgrade_from = NULL;
+
+		// Get old data
+		$query = $wpdb->prepare("
+			SELECT post_id, meta_value
+			FROM $wpdb->postmeta
+			WHERE meta_key = '%s'
+		", $this->key);
+
+		$old_data = $wpdb->get_results($query, 'ARRAY_A');
+
+		if ( !$old_data )
+			return 0;
+
+		// Convert old data
+		foreach ($old_data as $row) {
+			$id = $row['post_id'];
+
+			$old_element = unserialize(unserialize($row['meta_value']));
+
+			foreach($this->data as $field => $value)
+				$new_element[$field] = $old_element["cfi-$field"];
+
+			$count += update_post_meta($id, $this->key, $new_element);
+		}
+
+		return (int) $count;
+	}
+
+	function upgrade_1_2() {
+		global $wpdb;
+
+		// Set data fields
+		foreach ( $this->data as $name => $value )
+			$fields[] = "'cfi-$name'";
+
+		$fields = implode(',', $fields);
+
+		// Get old data
+		$query = $wpdb->prepare("
+			SELECT post_id, meta_key, meta_value
+			FROM $wpdb->postmeta
+			WHERE meta_key IN(%s)
+		", $fields);
+
+		$old_data = $wpdb->get_results($query, 'ARRAY_A');
+
+		if ( !$old_data )
+			return 0;
+
+		// Convert old data
+		foreach ($old_data as $row) {
+			$id = $row['post_id'];
+			$new_field = substr($row['meta_key'], 4);
+
+			$new_data[$id][$new_field] = $row['meta_value'];
+		}
+
+		// Add new data (don't overwrite newer data)
+		foreach ($new_data as $id => $element)
+			$count += add_post_meta($id, $this->key, $element, TRUE);
+
+		// Delete old data
+		$query = $wpdb->prepare("
+			DELETE FROM $wpdb->postmeta
+			WHERE meta_key IN(%s)
+		", $fields);
+
+		$wpdb->query($query);
+
+		return (int) $count;
+	}
+
+// Import/Export methods
+
+	function impex($action) {
+		$operators = array(
+			'import' => '!=',
+			'export' => '='
+		);
+
+		$posts = $this->get_posts($operators[$action]);
+
+		foreach ( $posts as $post )
+			$count += call_user_func(array(&$this, $action . '_single'), $post);
+
+		return (int) $count;
+	}
+
+	function import_single($post) {
+		if ( 0 == preg_match('#^\s*(<a[^\<]+>)?\s*(<img[^\<]+>)\s*(?:</a>)?#i', $post->content, $matches) )
+			return 0;
+
+		$img = $this->get_attributes($matches[2]);
+
+		$element['url'] = $img['src'];
+		$element['alt'] = $img['alt'];
+
+		// Set align
+		$img_clases = explode(' ', $img['class']);
+
+		foreach ( $img_clases as $class ) {
+			if ( !in_array(substr($class, 5), array_keys($this->styles)) )
+				continue;
+
+			$align = substr($class, 5);
+			break;
+		}
+
+		$element['align'] = $align;
+
+		// Set link
+		$element['link'] = '';
+
+		if ( $matches[1] ) {
+			$link = $this->get_attributes($matches[1]);
+			$element['link'] = $link['href'];
+		}
+
+		add_post_meta($post->ID, $this->key, $element, TRUE);
+
+		// Delete image from post
+		$new_content = str_replace($matches[0], '', $post->content);
+
+		if ( $new_content == $post->content )
+			return 0;
+
+		$this->update_post($new_content, $post->ID);
+
+		return 1;
+	}
+
+	function export_single($post) {
+		$new_content = $this->generate($post->ID) . $post->content;
+
+		if ( $new_content == $post->content )
+			return 0;
+
+		$this->update_post($new_content, $post->ID);
+
+		delete_post_meta($post->ID, $this->key);
+
+		return 1;
+	}
+
+	function get_attributes($string) {
+		preg_match_all('#(\w+)="\s*((?:[^"]+\s*)+)\s*"#i', $string, $matches, PREG_SET_ORDER);
+
+		foreach( $matches as $att )
+			$attributes[$att[1]] = $att[2];
+
+		return $attributes;
+	}
+
+	function get_posts($operator) {
+		global $wpdb;
+
+		$query = $wpdb->prepare("
+			SELECT DISTINCT ID, post_content AS content
+			FROM $wpdb->posts NATURAL JOIN $wpdb->postmeta
+			WHERE post_status IN('publish', 'draft')
+			AND meta_key $operator '%s'
+		", $this->key);
+
+		return $wpdb->get_results($query);
+	}
+
+	function update_post($content, $id) {
+		global $wpdb;
+
+		$query = $wpdb->prepare("
+				UPDATE $wpdb->posts
+				SET post_content = %s
+				WHERE ID = %d
+		", $content, $id);
+
+		return $wpdb->query($query);
+	}
+
+// Delete methods
+
+	function delete() {
+		global $wpdb;
+
+		$query = $wpdb->prepare("
+			DELETE FROM $wpdb->postmeta
+			WHERE meta_key = '%s'
+		", $this->key);
+
+		return $wpdb->query($query);
+	}
+
+// Box methods
+
+	function box_init() {
+		add_meta_box('cfi-box', 'Custom Field Image', array(&$this, 'box'), 'post', 'normal');
+		add_meta_box('cfi-box', 'Custom Field Image', array(&$this, 'box'), 'page', 'normal');
+	}
+
+	function box() {
+		$this->load();
+?>
+		<table style="width: 100%;">
+		    <tr>
+		        <td style="width: 10%; text-align: right"><strong>Image URL</strong></td>
+		        <td><input tabindex="3" name="url" type="text" style="width: 100%" value="<?php echo $this->data['url']; ?>" /></td>
+		    </tr>
+		    <tr>
+		        <td style="width: 10%; text-align: right">Alt. Text</td>
+		        <td><input tabindex="3" name="alt" type="text" style="width: 100%" value="<?php echo $this->data['alt']; ?>" /></td>
+		    </tr>
+		    <tr>
+		        <td style="width: 10%; text-align: right">Link to</td>
+		        <td><input tabindex="3" name="link" type="text" style="width: 100%" value="<?php echo $this->data['link']; ?>" /></td>
+		    </tr>
+		    <tr>
+		        <td style="width: 10%; text-align: right">Align</td>
+		        <td><?php
+		        	foreach ( $this->styles as $align => $style ) {
+						echo '<input tabindex="3" name="align" type="radio" value="' . $align . '" ';
+						if ( $this->data['align'] == $align )
+							echo 'checked="checked" ';
+						echo '/>'. $align ."\n";
+					}
+				?></td>
+		    </tr>
+		</table>
+<?php
+	}
+
+	function save($post_id, $post) {
+		if ( $post->post_type == 'revision' )
+			return;
+
+		if ( $_POST['url'] == '' ) {
+			delete_post_meta($post_id, $this->key);
+			return;
+		}
+
+		foreach ( $this->data as $name => $value )
+			$this->data[$name] = $_POST[$name];
+
+		   add_post_meta($post_id, $this->key, $this->data, TRUE) or
+		update_post_meta($post_id, $this->key, $this->data);
+	}
+
+// Options and management page methods
+
+	function page_init() {
+		if ( current_user_can('manage_options') )
+			add_options_page('Custom Field Images', 'Custom Field Images', 8, 'custom-field-images', array(&$this, 'options_page'));
+			add_management_page('Custom Field Images', 'Custom Field Images', 8, 'custom-field-images', array(&$this, 'management_page'));
+	}
+
+	function update_options() {
+		$this->options = get_option('cfi_options');
+
+		if ( !isset($_POST['action']) )
+			return;
+
+		// Update options
+		if ( 'Save Changes' == $_POST['action'] ) {
+			check_admin_referer($this->nonce);
+
+			foreach ( $this->options as $name => $value )
+				$new_options[$name] = $_POST[$name];
+
+			if ( $this->options != $new_options ) {
+				$this->options = $new_options;
+				update_option('cfi_options', $this->options);
+			}
+
+			echo '<div class="updated fade"><p>Options <strong>saved</strong>.</p></div>';
+		}
+	}
+
+	function options_page() {
+		$this->update_options();
+?>
+<div class="wrap">
+
+<h2>Custom Field Images Options</h2>
+
+<form method="post" action="">
+	<table class="form-table">
+		<tr>
+			<th scope="row" valign="top">Display in</th>
+			<td>
+			<?php foreach ( array('content', 'excerpt', 'feed') as $name ) { ?>
+				<input type="checkbox" name="<?php echo $name; ?>" value="TRUE" <?php if ( $this->options[$name] == TRUE) echo 'checked="checked"'; ?> />
+			 	<label>post <?php echo $name; ?></label>
+				<br class="clear" />
+			<?php } ?>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top">Default alignment</th>
+			<td>
+			<?php foreach ( $this->styles as $align => $style ) { ?>
+				<input type="radio" name="default_align" value="<?php echo $align; ?>" <?php if ( $this->options['default_align'] == $align) echo 'checked="checked" ';?> />
+				<label><?php echo $align; ?></label>
+			<?php } ?>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top">Link image to the current post</th>
+			<td>
+				<input type="checkbox" name="default_link" value="TRUE" <?php if ( $this->options['default_link']) echo 'checked="checked" ';?> />
+				<label>If an image has no link specified, it will be linked to the post or page it is associated with.</label>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top">Extra attributes</th>
+			<td>
+				<input type="text" name="extra_attr" value="<?php echo htmlentities(stripslashes($this->options['extra_attr'])); ?>" style="width: 250px" />
+				<label>Example: <em>target="_blank" rel="nofollow"</em></label>
+				<p>This is for adding extra attributes to the links added to images.</p>
+			</td>
+		</tr>
+	</table>
+
+	<?php wp_nonce_field($this->nonce); ?>
+
+	<p class="submit">
+		<input name="action" type="submit" class="button-primary" value="Save Changes" />
+	</p>
+</form>
+</div>
+<?php
+	}
+
+	function management_page() {
+		if ( isset($_POST['action']) ) {
+			check_admin_referer($this->nonce);
+			$this->process_posts($_POST['action']);
+		}
+?>
+<div class="wrap">
+
+<p>Here you can manage all custom field images at once. Please make a <strong>backup</strong> of your database before you proceed.</p>
+
+<?php if ( $this->upgrade_from ) { ?>
+<h2>Upgrade custom field keys</h2>
+
+<p>This operation is required only once, in order to use older data.</p>
+
+<form method="post" action="">
+	<?php wp_nonce_field($this->nonce); ?>
+
+	<p class="submit">
+	<input name="action" type="submit" value="Upgrade" />
+	</p>
+</form>
+<br />
+<?php } ?>
+
+<h2>Import images</h2>
+
+<p>This will scan for images at beginning of posts, insert them into custom field keys and then remove them from the posts.</p>
+
+<form method="post" action="">
+	<?php wp_nonce_field($this->nonce); ?>
+
+	<p class="submit">
+		<input name="action" type="submit" onClick="return confirm('Are you sure you want to do this?\nIt cannot be undone.')" value="Import" />
+	</p>
+</form>
+<br />
+
+<h2>Export images</h2>
+
+<p>This will insert all custom field images at the beginning of their respective posts and then delete the custom field keys.</p>
+
+<form method="post" action="">
+	<?php wp_nonce_field($this->nonce); ?>
+
+	<p class="submit">
+		<input name="action" type="submit" onClick="return confirm('Are you sure you want to do this?\nIt cannot be undone.')" value="Export" />
+	</p>
+</form>
+<br />
+
+<h2>Delete images</h2>
+
+<p>This will delete all custom field images.</p>
+
+<form method="post" action="">
+	<?php wp_nonce_field($this->nonce); ?>
+
+	<p class="submit">
+		<input name="action" type="submit" onClick="return confirm('Are you sure you want to do this?\nIt cannot be undone.')" value="Delete" />
+	</p>
+</form>
+
+</div>
+<?php
+	}
+}
+
