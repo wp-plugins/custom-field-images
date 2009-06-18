88049fb72126846eda4523ee351ab8fc07d958e8
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-18 01:13:17 +0000

    1.9.2: bugfix
    git-svn-id: https://plugins.svn.wordpress.org/custom-field-images/trunk@126858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-field-images.php
+++ custom-field-images.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Custom Field Images
 Description: Easily associate any image to a post and display it in post excerpts, feeds etc.
-Version: 1.9.1
+Version: 1.9.2
 Author: scribu
 Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/custom-field-images
