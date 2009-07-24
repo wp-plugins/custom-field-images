7321305515a37a32d8f7cc4a7120218bd7434ca8
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-24 21:33:42 +0000

    starting 2.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-field-images/tags/2.0.0.1@138970 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-field-images.php
@@ -0,0 +1,235 @@
+<?php
+/*
+Plugin Name: Custom Field Images
+Description: Easily associate any image to a post and display it in post excerpts, feeds etc.
+Version: 2.0.0.1
+Author: scribu
+Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/wordpress/custom-field-images
+Text Domain: custom-field-images
+Domain Path: /lang
+
+Copyright (C) 2009 scribu.net (scribu AT gmail DOT com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+// Init
+_cfi_init();
+function _cfi_init()
+{
+	// Load translations
+	$plugin_dir = basename(dirname(__FILE__));
+	load_plugin_textdomain('custom-field-images', "wp-content/plugins/$plugin_dir/lang", "$plugin_dir/lang");
+
+	// Load scbFramework
+	require_once dirname(__FILE__) . '/scb/load.php';
+
+	$options = new scbOptions('cfi_options', __FILE__, array(
+		'default_url' => '',
+		'default_align' => '',
+		'default_link' => TRUE,
+		'extra_attr' => '',
+		'insert_button' => TRUE,
+
+		'content' => TRUE,
+		'feed' => TRUE,
+		'excerpt' => TRUE
+	));
+
+	displayCFI::init($options);
+
+	// Load template tags
+	require_once dirname(__FILE__) . '/template-tags.php';
+
+	// Load widget class
+	if ( class_exists('WP_Widget') )
+	{
+		require_once dirname(__FILE__) . '/widget.php';
+		scbWidget::init('widgetCFI', __FILE__, 'cfi-loop');
+	}
+
+	// Load admin classes
+	if ( is_admin() )
+	{
+		require_once dirname(__FILE__) . '/admin.php';
+		cfi_admin_init(__FILE__, $options);
+	}
+}
+
+abstract class displayCFI
+{
+	// wp_postmeta.meta_key
+	const key = '_cfi_image';
+
+	const token = '[cfi]';
+
+	// Styles for images in feeds
+	static $styles = array(
+		'left' => 'float:left; margin: 0 1em .5em 0;',
+		'center' => 'display:block; margin:0 auto .5em auto;',
+		'right' => 'float:right; margin: 0 0 .5em 1em;'
+	);
+
+	// Data fields for current image
+	static $data = array(
+		'id' => '',
+		'size' => '',
+		'url' => '',
+		'align' => '',
+		'alt' => '',
+		'link' => ''
+	);
+
+	// $post->ID
+	static $id;
+
+	// Options object holder
+	static $options;
+
+	static function init($options)
+	{
+		self::$options = $options;
+
+		add_filter('the_excerpt', array(__CLASS__, 'filter'), 20);
+		add_filter('the_content', array(__CLASS__, 'filter'), 20);
+	}
+
+	static function filter($content)
+	{
+		$type = substr(current_filter(), 4);
+		$is_feed = is_feed();
+
+		$cond = ($is_feed && self::$options->feed) || (!$is_feed && self::$options->$type);
+
+		if ( !$cond )
+			return $content;
+
+		$img = self::generate();
+
+		if ( $type != 'excerpt' && FALSE !== strpos($content, self::token) )
+			$content = str_replace(self::token, $img, $content);
+		else
+			$content = $img . $content;
+
+		// For partial feeds
+//		if ( is_feed() )
+//			echo $img;
+
+		return $content;
+	}
+
+	static function generate($post_id = '', $defaults = '')
+	{
+		self::load($post_id);
+
+		if ( ! empty($defaults) )
+			self::$data = wp_parse_args($defaults, self::$data);
+
+		if ( self::$data['id'] )
+		{
+			if ( self::$data['size'] )
+				$data = image_downsize(self::$data['id'], self::$data['size']);
+ 			else
+ 				$data = image_downsize(self::$data['id']);
+
+			$url = $data[0];
+		}
+
+		if ( empty($url) )
+			$url = self::$data['url'];
+
+		if ( empty($url) )
+			$url = self::$options->default_url;
+
+		if ( empty($url) )
+			return;
+
+		// Begin img tag
+		$image .= '<img src="'. $url .'" ';
+
+		// Set alignment
+		$align = self::$data['align'] ? self::$data['align'] : self::$options->default_align;
+
+		if ( is_feed() )
+			$image .= sprintf( 'style="%s" ', self::$styles[$align] );
+		else
+			$image .= sprintf( 'class="cfi align%s" ', $align );
+
+		// Set alt text & title
+		$alt = self::$data['alt'] ? self::$data['alt'] : get_the_title();
+
+		$image .= sprintf( 'alt="%s" title="%s"', $alt, $alt);
+
+		// End img tag
+		$image .= '/>';
+
+		return self::add_link($image);
+	}
+
+	static function loop($query)
+	{
+		$query = wp_parse_args($query, array(
+			'meta_key' => displayCFI::key,
+			'post_type' => 'post',
+			'post_status' => 'publish'
+		));
+
+		$side_query = new WP_query($query);
+
+		ob_start();
+
+		// Do the loop
+		echo "<ul class='cfi-loop'>";
+		while ( $side_query->have_posts() ) : $side_query->the_post();
+			echo "<li>";
+			echo self::generate($post->ID, array(
+				'size' => 'thumbnail',
+				'alt' => $post->post_title,
+				'align' => '',
+				'link' => get_permalink($post->ID)
+			));
+			echo "</li>\n";
+		endwhile;
+		echo "</ul>\n";
+
+		return ob_get_clean();
+	}
+
+	protected static function add_link($image)
+	{
+		$link = self::$data['link'];
+
+		if ( empty($link) )
+			if ( self::$options->get('default_link') )
+				$link = get_permalink(self::$id);
+			else
+				return $image . "\n";
+
+		return sprintf( "<a href='$link' %s>$image</a>\n", stripslashes(self::$options->get('extra_attr')) );
+	}
+
+	static function load($post_id = '')
+	{
+		global $post;
+
+		$post_id = intval($post_id);
+
+		self::$id = $post_id ? $post_id : $post->ID;
+
+		self::$data = get_post_meta(self::$id, self::key, TRUE);
+	}
+}
+
---
+++ inc/insert.js
@@ -0,0 +1,72 @@
+jQuery(document).ready(function($){
+	var $box = $(document).find('#cfi-box');
+	var $id_row = $box.find('tr:eq(0)');
+	var $url_row = $box.find('tr:eq(1)');
+
+	function fillCfiBox()
+	{
+		var $item = $(this).parents('.media-item');
+
+		$id_row.show();
+		$url_row.hide();
+
+		// Set cfi-id
+		var id = $item.find(':submit').attr('name').replace(/^.*?(\d+).*?$/, '$1');
+		$box.find('[name=cfi-id]').val(id);
+
+		// Set cfi-size
+		var size = $item.find('.image-size :checked').val();
+		$box.find('[name=cfi-size]').val(size);
+
+		// Set cfi-align
+		var align = $item.find('.align :checked').val();
+		if ( align != 'none' )
+			$box.find('tr:last [value="'+align+'"]').attr('checked',true);
+		else
+			$box.find('tr:last :checked').attr('checked', false);	// uncheck all buttons
+
+		// Set cfi-alt
+		var alt = $item.find('.post_title :text').val();
+		$box.find('[name=cfi-alt]').val(alt);
+
+		// Set cfi-link
+		var link = $item.find('.url :text').val();
+		$box.find('[name=cfi-link]').val(link);
+
+		$(document).find('#TB_closeWindowButton').click();		// close iframe
+
+		return false;
+	}
+
+	function addButton()
+	{
+		var button = $('<a>')
+			.attr('href', '#')
+			.css('color', '#006505')
+			.text(cfiL10n.insert_text)
+			.addClass('insert-cfi')
+			.click(fillCfiBox);
+
+		$('.media-item :submit', $(this).contents()).livequery(function() {
+			if ( $(this).find(' + .insert-cfi').length > 0 )
+				return;
+
+			button.clone(true)
+				.insertAfter($(this))
+				.before(' &nbsp;(')
+				.after(')');
+		});
+	}
+
+	if ( $id_row.find(':text').val() == "" )
+		$id_row.hide();
+	else
+		$url_row.hide();
+
+	$('#add_image').click(function() {										// when invoking iframe
+		$('#TB_iframeContent').livequery(function() {						// after creating iframe tag
+			$('#TB_iframeContent').load(addButton); 						// after each tab load
+		});
+	});
+});
+
